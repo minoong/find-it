f5e2d7f6ac3dd4eb7b4f810825519c857143c106
minoong <alsdndjdrktl@gmail.com>
2021-02-09 22:14:16 +0900

    signup start...

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import CloseXIcon from '../../public/static/svg/modal/modal_colose_x_icon.svg';
+import MailIcon from '../../public/static/svg/auth/mail.svg';
+import PersonIcon from '../../public/static/svg/auth/person.svg';
+import OpenedEyeIcon from '../../public/static/svg/auth/opened_eye.svg';
+import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
+import palette from '../../styles/palette';
 
 const SignUpModalBlock = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 35.5rem;
   height: 38.375rem;
   background-color: white;
-  padding: 0.5rem;
+  padding: 2rem;
   z-index: 11;
   transform: scale(0);
   animation: zoomIn 0.2s 0s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+  border: 1px solid ${palette.gray_aa};
+
+  .modal-close-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+    path {
+      fill: ${palette.gray_aa};
+    }
+    :hover path {
+      fill: ${palette.black};
+    }
+  }
+
+  div {
+    margin: 5px;
+  }
 
   @keyframes zoomIn {
     from {
@@ -21,7 +46,16 @@ const SignUpModalBlock = styled.div`
 `;
 
 const SignUpModal: React.FC = () => {
-  return <SignUpModalBlock>Sign Up</SignUpModalBlock>;
+  return (
+    <SignUpModalBlock>
+      <CloseXIcon className="modal-close-icon" />
+      <div>sdafasd</div>
+      <div>sdafasd</div>
+      <div>sdafasd</div>
+      <div>sdafasd</div>
+      <div>sdafasd</div>
+    </SignUpModalBlock>
+  );
 };
 
 export default SignUpModal;
