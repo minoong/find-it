724900a48c3005c8289b1a77fd67933d2c556446
minoong <alsdndjdrktl@gmail.com>
2021-02-24 21:04:22 +0900

    프로필 메뉴 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,13 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import Link from 'next/link';
+import OutsideClickHandler from 'react-outside-click-handler';
 import palette from '../styles/palette';
 import AuthModal from './auth/AuthModal';
 import useModal from '../hooks/useModal';
-import user from '../lib/data/user';
 import { useSelector } from '../store';
 import HamburgerIcon from '../public/static/svg/header/hamburger.svg';
 import { authActions } from '../store/auth';
-import SignUpModal from './auth/SignUpModal';
 
 const HeaderBlock = styled.div`
   position: sticky;
@@ -39,6 +38,41 @@ const HeaderBlock = styled.div`
       }
     }
   }
+
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 3.25rem;
+    width: 10rem;
+    padding: 0.5rem 0;
+    box-shadow: 0 2px 1rem rgba(0, 0, 0, 0.12);
+    border-radius: 0.5rem;
+    background-color: white;
+    li {
+      font-size: 0.5rem;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      line-height: 2.5;
+      padding: 0 1rem;
+      cursor: pointer;
+      transition: all 0.1s ease-out;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 0.5rem 0;
+      background-color: ${palette.gray_dd};
+    }
+  }
+
   .header-auth-buttons {
     .header-sign-up-button {
       height: 2.625rem;
@@ -102,6 +136,7 @@ const Header: React.FC = () => {
   const dispatch = useDispatch();
   const { openModal, ModalPortal, closeModal } = useModal();
   const user = useSelector((state) => state.user);
+  const [isUserMenuOpend, setIsuserMenuOpend] = useState(false);
   return (
     <>
       <HeaderBlock>
@@ -135,10 +170,35 @@ const Header: React.FC = () => {
           </div>
         )}
         {user.isLogged && (
-          <button className="header-user-profile" type="button" onClick={() => {}}>
-            <HamburgerIcon />
-            <img src={user.profileImage} className="header-user-profile-image" alt="" />
-          </button>
+          <OutsideClickHandler
+            onOutsideClick={() => {
+              if (isUserMenuOpend) setIsuserMenuOpend(false);
+            }}
+          >
+            <button className="header-user-profile" type="button" onClick={() => setIsuserMenuOpend(!isUserMenuOpend)}>
+              <HamburgerIcon />
+              <img src={user.profileImage} className="header-user-profile-image" alt="" />
+            </button>
+            {isUserMenuOpend && (
+              <ul className="header-usermenu">
+                <li>호스텔 관리</li>
+                <Link href="/room/register/building">
+                  <a
+                    role="presentation"
+                    onClick={() => {
+                      setIsuserMenuOpend(false);
+                    }}
+                  >
+                    <li>호스텔 등록</li>
+                  </a>
+                </Link>
+                <div className="header-usermenu-divider" />
+                <li role="presentation" onClick={() => {}}>
+                  로그아웃
+                </li>
+              </ul>
+            )}
+          </OutsideClickHandler>
         )}
         <ModalPortal>
           <AuthModal closeModal={closeModal} />
