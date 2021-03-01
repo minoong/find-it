902ad96143aa4258cfeaa60ece640ba18884e80f
minoong <alsdndjdrktl@gmail.com>
2021-03-01 21:18:46 +0900

    step implements

--- components/common/RadioGroup.tsx
+++ components/common/RadioGroup.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable indent */
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { useSelector } from '../../store';
 import palette from '../../styles/palette';
+import WarningIcon from '../../public/static/svg/common/warning.svg';
 
 const RadioGroupBlock = styled.div<{ isValid: boolean; validationMode: boolean }>`
   .radio-label {
-    font-size: 1rem;
+    font-size: 16px;
     font-weight: 600;
-    color: ${palette.gray_80};
-    margin-bottom: 2rem;
+    color: ${palette.gray_76};
+    margin-bottom: 32px;
   }
   .radio-list-wrapper {
     &:after {
@@ -19,7 +21,8 @@ const RadioGroupBlock = styled.div<{ isValid: boolean; validationMode: boolean }
   }
   label {
     float: left;
-    margin-bottom: 1.5rem;
+    margin-bottom: 24px;
+    font-size: 16px;
     line-height: 1.2;
     cursor: pointer;
     clear: both;
@@ -30,15 +33,16 @@ const RadioGroupBlock = styled.div<{ isValid: boolean; validationMode: boolean }
   }
 
   input[type='radio'] {
-    width: 1rem;
-    height: 1rem;
+    width: 16px;
+    height: 16px;
     margin: 0;
     position: relative;
-    margin-right: 0.75rem;
+    margin: 0;
+    margin-right: 12px;
     flex-shrink: 0;
-    font-size: 1rem;
+    font-size: 16px;
     -webkit-appearance: none;
-    border: 1ps solid ${palette.gray_b0};
+    border: 1px solid ${palette.gray_b0};
     border-radius: 50%;
     outline: none;
     cursor: pointer;
@@ -58,10 +62,87 @@ const RadioGroupBlock = styled.div<{ isValid: boolean; validationMode: boolean }
       return undefined;
     }}
   }
+
+  input[type='radio']:checked {
+    background-color: ${palette.dark_cyan};
+    border: 0;
+  }
+  input[type='radio']:checked:after {
+    content: '';
+    width: 6px;
+    height: 6px;
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: white;
+    border-radius: 50%;
+    display: block;
+  }
+
+  .radio-description {
+    display: block;
+    margin-top: 5px;
+    margin-left: 28px;
+  }
+
+  .radio-group-warning {
+    margin-top: 8px;
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 4px;
+    }
+    p {
+      font-size: 12px;
+      color: ${palette.davidson_orange};
+    }
+  }
 `;
 
-const RadioGroup: React.FC = () => {
-  return <RadioGroupBlock></RadioGroupBlock>;
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  value?: any;
+  onChange?: (value: any) => void;
+  options?: { label: string; value: any; description?: string }[];
+  isValid?: boolean;
+  errorMessage?: string;
+}
+
+const RadioGroup: React.FC<IProps> = ({
+  label,
+  value,
+  options = [],
+  onChange,
+  isValid,
+  errorMessage = '옵션을 선택하세요.',
+}) => {
+  const validationMode = useSelector((state) => state.common.validationMode);
+  return (
+    <RadioGroupBlock isValid={!!isValid} validationMode={validationMode}>
+      <p className="radio-label">{label}</p>
+      <div className="radio-list-wrapper">
+        {options.map((option, index) => (
+          <label key={index}>
+            <input type="radio" checked={value === option.value} onChange={() => onChange && onChange(option.value)} />
+            <span>
+              {option.label}
+              <span className="radio-description">{option.description}</span>
+            </span>
+          </label>
+        ))}
+      </div>
+      {validationMode && !isValid && (
+        <div className="radio-group-warning">
+          <WarningIcon />
+          <p>{errorMessage}</p>
+        </div>
+      )}
+    </RadioGroupBlock>
+  );
 };
 
 export default RadioGroup;
--- components/register/RegisterRoomBuilding.tsx
+++ components/register/RegisterRoomBuilding.tsx
@@ -5,6 +5,7 @@ import { apartmentBuildingTypeList, largeBuildingTypeList } from '../../lib/stat
 import { useSelector } from '../../store';
 import { registerRoomActions } from '../../store/registerRoom';
 import palette from '../../styles/palette';
+import RadioGroup from '../common/RadioGroup';
 import Selector from '../common/Selector';
 
 const RegisterRoomBuildingBlock = styled.div`
@@ -23,19 +24,46 @@ const RegisterRoomBuildingBlock = styled.div`
     width: 20rem;
     margin-bottom: 2rem;
   }
+  .register-room-room-type-radio {
+    max-width: 30.3125rem;
+    margin-bottom: 3.125rem;
+  }
 `;
 
 const disabledLargeBuildingTypeOptions = ['숙소를 선택하세요.'];
+const roomTypeRadioOptions = [
+  {
+    label: '집 전체',
+    value: 'entire',
+    description:
+      '게스트가 숙소 전체를 다른 사람과 공유하지 않고 단독으로 이용합니다. 일반적으로 침실, 욕실, 부엌이 포함됩니다.',
+  },
+  {
+    label: '개인실',
+    value: 'private',
+    description: '게스트에게 개인 침실이 제공됩니다. 침실 이외의 공간은 공용일 수 있습니다.',
+  },
+  {
+    label: '다인실',
+    value: 'public',
+    description: '게스트는 개인 공간 없이, 다른 사람과 함께 쓰는 침실이나 공용공간에서 숙박합니다.',
+  },
+];
 
 const RegisterRoomBuilding: React.FC = () => {
   const largeBuildingType = useSelector((state) => state.registerRoom.largeBuildingType);
   const buildingType = useSelector((state) => state.registerRoom.buildingType);
+  const roomType = useSelector((state) => state.registerRoom.roomType);
+
   const dispatch = useDispatch();
   const onChageLargeBuildingType = (event: React.ChangeEvent<HTMLSelectElement>) => {
     dispatch(registerRoomActions.setLargeBulidingType(event.target.value));
   };
   const onChageBuildingType = (event: React.ChangeEvent<HTMLSelectElement>) => {
     dispatch(registerRoomActions.setBulidingType(event.target.value));
+  };
+  const onChangeRoomType = (selected: string) => {
+    dispatch(registerRoomActions.setRoomType(selected as 'entire' | 'private' | 'public'));
   };
   const detailBuildingOptions = useMemo(() => {
     switch (largeBuildingType) {
@@ -97,6 +125,16 @@ const RegisterRoomBuilding: React.FC = () => {
           options={detailBuildingOptions}
         />
       </div>
+      {buildingType && (
+        <div className="register-room-room-type-radio">
+          <RadioGroup
+            label="게스트가 묵게 될 숙소 유형을 골라주세요."
+            value={roomType}
+            options={roomTypeRadioOptions}
+            onChange={onChangeRoomType}
+          />
+        </div>
+      )}
     </RegisterRoomBuildingBlock>
   );
 };
